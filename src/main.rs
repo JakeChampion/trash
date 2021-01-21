6afcc81e2f1dd219e314a168b1b57c2d183ce224
jake champion <me@jakechampion.name>
2021-01-21 02:23:15 +0000

    implement glob support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use glob::glob;
 use std::path::PathBuf;
 use trash;
 
 fn main() {
     let app = clap::App::new("trash");
+    let app = app.version("1.0.0");
     let app = app.about("Move files and folders to the trash");
     let app = app.arg(
         clap::Arg::with_name("files")
@@ -21,9 +23,13 @@ fn main() {
             .hidden(true)
             .required(false),
     );
-    let app = app.version("0.1.0");
-    let files: Vec<PathBuf> = app.get_matches().values_of_os("files")
-    .map_or_else(Vec::new, |v| v.map(::std::convert::From::from).collect());
-    let files: Vec<&PathBuf> = files.iter().filter(|v| v.exists()).collect();
-    trash::delete_all(files).unwrap();
+    let globs: Vec<String> = app
+        .get_matches()
+        .values_of("files")
+        .map_or_else(Vec::new, |v| v.map(::std::convert::From::from).collect());
+
+    for pattern in globs {
+        let paths: Vec<PathBuf> = glob(&pattern).unwrap().filter_map(Result::ok).collect();
+        trash::delete_all(paths).unwrap();
+    }
 }
